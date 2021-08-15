312cd69dd68d4f52c37661e1fbaf7205ea69a6ab
walaamohammad <layan_abadi86@yahoo.com>
2021-08-15 08:04:58 +0300

    add cache

--- controllers/Movie.controllers.js
+++ controllers/Movie.controllers.js
@@ -1,12 +1,29 @@
 'use strict';
 
 const Result =require("../models/Movie.models")
+const Cache = require('../helper/cache.helper');
+let cacheObject = new Cache();
 const axios = require('axios');
 
 
 const movieController=(req,res)=>{
     let city = req.params.city_name;
     let movieUrl = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.MOVIES_API_KEY}&query=${city}`
+
+    
+  cacheObject.aflam.push({
+    "query": query,
+    "data": callMovies
+  })
+  return callMovies;
+};
+if((Date.now() - cacheObject.timeStamp) > 86400000){ // reset the cache object if the timeStamp exeeds 1 day
+  cacheObject = new Cache();
+}
+if(cacheObject.aflam.length){
+ const filteredData = cacheObject.aflam.find((movie) =>{
+   return movie.query === query;
+ });
     
     let newFilms=[]
     axios.get(movieUrl).then(moveisData=>{
---
+++ helper/cache.helper.js
@@ -0,0 +1,7 @@
+class Cache {
+    constructor(data){
+      this.timeStamp = Date.now(); // initialize time to start use data 
+      this.aflam=[];
+    }
+}
+module.exports= Cache; 
